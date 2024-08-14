c237de614c6739d1f39d7dc56b21f0c5c1135f23
Rupesh Singla <122483245+rupesh0105@users.noreply.github.com>
2024-08-14 23:45:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "441aaa359d2ed9740083c3814cac772e";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weathericon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humid").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weathericon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Rain") {
+      weathericon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Clear") {
+      weathericon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Snow") {
+      weathericon.src = "images/snow.png";
+    } else if (data.weather[0].main == "Wind") {
+      weathericon.src = "images/wind.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weathericon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weathericon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
